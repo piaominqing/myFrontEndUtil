d987a602f78acaf73c5dbc23f0962f4885862505
pmq <piao_minqing@nhn-st.com>
2019-11-19 17:20:36 +0800

    add queue logic

--- src/js/util/Queue/queue.js
+++ src/js/util/Queue/queue.js
@@ -1,6 +1,6 @@
 // 创建队列（为了获取元素更高效，采用对象来存储我们的元素）
-// count 队列的大小
-// lowestCount 追踪第一个元素
+// count 目前为止队列总元素数
+// lowestCount 队列当前元素数
 // items 存储元素
 export class Queue {
   constructor() {
@@ -39,5 +39,30 @@ export class Queue {
     }
 
     return this.items[this.lowestCount];
+  }
+
+  // 获取队列长度
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  // 清空队列
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+
+  // toString
+  toString() {
+    if (this.isImpty()) {
+      return null;
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i += 1) {
+      objString = `${objString},${this.items[i]}`;
+    }
+
+    return objString;
   }
 }
