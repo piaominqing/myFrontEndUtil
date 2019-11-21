d0211fc0d5d9d52d14f7f6eb4538f8b8328bd510
pmq <piao_minqing@nhn-st.com>
2019-11-21 15:32:59 +0800

    add Set logic

---
+++ src/js/util/set/set.js
@@ -0,0 +1,41 @@
+export class Set {
+  constructor() {
+    this.items = {};
+  }
+
+  has(element) {
+    return Object.prototype.hasOwnProperty.call(this.items, element);
+  }
+
+  add(element) {
+    if (!this.has(element)) {
+      this.items[element] = element;
+
+      return true;
+    }
+
+    return false;
+  }
+
+  delete(element) {
+    if (this.has(element)) {
+      delete this.items[element];
+
+      return true;
+    }
+
+    return false;
+  }
+
+  clear() {
+    this.items = {};
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  values() {
+    return Object.values(this.items);
+  }
+}
