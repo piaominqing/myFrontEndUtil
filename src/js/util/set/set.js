38b2e1e1037b9488c192e6d463ed9bb838d40d6e
pmq <piao_minqing@nhn-st.com>
2019-11-21 16:51:59 +0800

    add Set union logic

--- src/js/util/set/set.js
+++ src/js/util/set/set.js
@@ -38,4 +38,65 @@ export class Set {
   values() {
     return Object.values(this.items);
   }
+
+  // 集合并集
+  union(otherSet) {
+    const unionSet = new Set();
+    this.values().forEach(value => unionSet.add(value));
+    otherSet.values().forEach(value => unionSet.add(value));
+
+    return unionSet;
+  }
+
+  // 交集
+  intersection(otherSet) {
+    const intersectionSet = new Set();
+    const values = this.values();
+    const otherValues = otherSet.values();
+    let biggerSet = values;
+    let smallerSet = otherValues;
+    // 循环size少的那个set 优化性能
+    if (otherValues.length - values.length > 0) {
+      biggerSet = otherValues;
+      smallerSet = values;
+    }
+    smallerSet.forEach(value => {
+      if (biggerSet.includes(value)) {
+        intersectionSet.add(value);
+      }
+    });
+
+    return intersectionSet;
+  }
+
+  // 差集
+  difference(otherSet) {
+    const differenceSet = new Set();
+    this.values().forEach(value => {
+      if (!otherSet.has(value)) {
+        differenceSet.add(value);
+      }
+    });
+
+    return differenceSet;
+  }
+
+  // 子集
+  isSubSetOf(otherSet) {
+    if (this.size() > otherSet.size()) {
+      return false;
+    }
+    let isSubSet = true;
+    this.values().every(value => {
+      if (!otherSet.has(value)) {
+        isSubSet = false;
+
+        return false;
+      }
+
+      return true;
+    });
+
+    return isSubSet;
+  }
 }
