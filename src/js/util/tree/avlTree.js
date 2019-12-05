13c97d36f3617e0bff068a18b2e637075dc253e0
pmq <piao_minqing@nhn-st.com>
2019-12-05 17:03:01 +0800

    add avl tree logic

--- src/js/util/tree/avlTree.js
+++ src/js/util/tree/avlTree.js
@@ -1,5 +1,5 @@
 import {BinarySearchTree} from './binarySearchTree';
-import {defaultCompare} from '../nativeUtil';
+import {defaultCompare, Compare} from '../nativeUtil';
 export const BalanceFactor = {
   UNBALANCED_RIGHT: 1,
   SLIGHTLY_UNBALANCED_RIGHT: 2,
@@ -38,5 +38,115 @@ export class avlTree extends BinarySearchTree {
       default:
         return BalanceFactor.BALANCED;
     }
+  }
+
+  // 向右的单选转
+  rotationLL(node) {
+    const tmp = node.left;
+    node.left = tmp.right;
+    tmp.right = node;
+
+    return tmp;
+  }
+
+  // 向左的单选转
+  rotationRR(node) {
+    const tmp = node.right;
+    node.right = tmp.left;
+    tmp.left = node;
+
+    return tmp;
+  }
+
+  // 向右的双旋转
+  rotationLR(node) {
+    node.left = this.rotationRR(node.left);
+
+    return this.rotationLL(node);
+  }
+
+  // 向左的双旋转
+  rotationRL(node) {
+    node.right = this.rotationLL(node.right);
+
+    return this.rotationRR(node);
+  }
+
+  insert(key) {
+    this.root = this.insertNode(this.root, key);
+  }
+
+  insertNode(node, key) {
+    // 像在BST树中一样插入一个节点
+    if (node === null) {
+      return new Node(key);
+    }
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      node.left = this.insertNode(node.left, key);
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      node.right = this.insertNode(node.right, key);
+    } else {
+      return node;
+    }
+
+    // 如果需要，将树进行平衡操作
+    const balanceFactor = this.getBalanceFactor(node);
+    if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+      if (this.compareFn(key, node.left.key) === Compare.LESS_THAN) {
+        node = this.rotationLL(node);
+      } else {
+        return this.rotationLR(node);
+      }
+    }
+    if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+      if (this.compareFn(key, node.right.key) === Compare.BIGGER_THAN) {
+        node = this.rotationRR(node);
+      } else {
+        return this.rotationRL(node);
+      }
+    }
+
+    return node;
+  }
+
+  remove(key) {
+    this.root = this.removeNode(this.root, key);
+  }
+
+  removeNode(node, key) {
+    node = super.removeNode(node, key);
+    if (node === null) {
+      return node; // null 不需要平衡
+    }
+
+    // 检测树是否平衡
+    const balanceFactor = this.getBalanceFactor(node);
+    if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+      const balanceFactorLeft = this.getBalanceFactor(node.left);
+      if (
+        balanceFactorLeft === BalanceFactor.BALANCED ||
+        balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT
+      ) {
+        return this.rotationLL(node);
+      }
+      if (balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT) {
+        return this.rotationLR(node.left);
+      }
+    }
+
+    if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+      const balanceFactorRight = this.getBalanceFactor(node.right);
+      if (
+        balanceFactorRight === BalanceFactor.BALANCED ||
+        balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT
+      ) {
+        return this.rotationRR(node);
+      }
+      if (balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT) {
+        return this.rotationRL(node.right);
+      }
+    }
+
+    return node;
   }
 }
